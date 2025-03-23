cc5a1dda4f47e1681461d330f1736cfb8be3701c
Amit Yadav <59335027+starlordamit@users.noreply.github.com>
2025-03-23 21:38:06 +0530

    Update server.py

--- src/api/server.py
+++ src/api/server.py
@@ -37,6 +37,30 @@ def login():
         return (resp.text, resp.status_code, {'Content-Type': 'application/json'})
     except Exception as e:
         return jsonify({"error": str(e)}), 500
+@app.route('/trending', methods=['GET'])
+def trending():
+    url = "https://api.thumbnailpreview.com/api/youtube/trending"
+    headers = {
+        "accept": "*/*",
+        "accept-language": "en-US,en;q=0.9",
+        "if-none-match": "\"1357opnwisk2ipk-df\"",
+        "priority": "u=1, i",
+        "referer": "https://api.thumbnailpreview.com/v1",
+        "sec-ch-ua": "\"Chromium\";v=\"134\", \"Not:A-Brand\";v=\"24\", \"Google Chrome\";v=\"134\"",
+        "sec-ch-ua-mobile": "?0",
+        "sec-ch-ua-platform": "\"macOS\"",
+        "sec-fetch-dest": "empty",
+        "sec-fetch-mode": "cors",
+        "sec-fetch-site": "same-origin",
+        "user-agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/134.0.0.0 Safari/537.36"
+    }
+   
+    try:
+        resp = requests.get(url, headers=headers)
+        resp.raise_for_status()
+        return (resp.text, resp.status_code, {'Content-Type': 'application/json'})
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
